c0e5fe66da4f93a7c648be98ffa407588ee9db52
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-02-05 10:55:20 -0800

    🌱 working on seeding to heroku

--- seed.js
+++ seed.js
@@ -1,10 +1,6 @@
-if (process.env.MONGODB_URI) {
-	process.env.NODE_ENV = "prod"
-} else {
-	process.env.NODE_ENV = "dev"
-}
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 module.exports = {
-	"dev": "localhost/petes-pets",
-	"prod": "ds113855.mlab.com:13855/heroku_d1jkkzjb"
-}
+	dev: "localhost/petes-pets",
+	prod: process.env.MONGODB_URI,
+};
